cc8f174c26ebe0f50cf8a28dd8fa6e15698f0ad4
Elvan Hidayat <elvanhd@gmail.com>
2023-01-26 11:37:14 +0700

    create product model and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -20,11 +21,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        // User
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
+        // Category
         Category::factory()->create([
             'name' => 'Konsumsi',
             'slug' => Str::slug('Konsumsi'),
@@ -33,6 +36,42 @@ class DatabaseSeeder extends Seeder
         Category::factory()->create([
             'name' => 'Pembersih',
             'slug' => Str::slug('Pembersih'),
+        ]);
+
+        // Product
+        Product::factory()->create([
+            'name' => 'Kopi',
+            'price' => 20000,
+            'quantity_in_stock' => 100,
+            'category_id' => 1,
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Teh',
+            'price' => 12500,
+            'quantity_in_stock' => 100,
+            'category_id' => 1,
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Pasta Gigi',
+            'price' => 9500,
+            'quantity_in_stock' => 100,
+            'category_id' => 2,
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Sabun Mandi',
+            'price' => 27500,
+            'quantity_in_stock' => 100,
+            'category_id' => 2,
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Sampo',
+            'price' => 22500,
+            'quantity_in_stock' => 100,
+            'category_id' => 2,
         ]);
     }
 }
