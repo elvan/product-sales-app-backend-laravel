1257f003d3f6976653395236377153da1c994b6f
Elvan Hidayat <elvanhd@gmail.com>
2023-01-26 15:34:00 +0700

    fix attribute name on the transaction seeder

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -18,49 +18,49 @@ class TransactionSeeder extends Seeder
             'customer_name' => 'Customer 1',
             'total_price' => 100000,
             'items_quantity' => 10,
-            'completed_at' => '2021-05-01 00:00:00',
+            'added_at' => '2021-05-01 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 2',
             'total_price' => 190000,
             'items_quantity' => 19,
-            'completed_at' => '2021-05-05 00:00:00',
+            'added_at' => '2021-05-05 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 3',
             'total_price' => 135000,
             'items_quantity' => 15,
-            'completed_at' => '2021-05-10 00:00:00',
+            'added_at' => '2021-05-10 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 4',
             'total_price' => 190000,
             'items_quantity' => 20,
-            'completed_at' => '2021-05-11 00:00:00',
+            'added_at' => '2021-05-11 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 5',
             'total_price' => 300000,
             'items_quantity' => 30,
-            'completed_at' => '2021-05-11 00:00:00',
+            'added_at' => '2021-05-11 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 6',
             'total_price' => 250000,
             'items_quantity' => 25,
-            'completed_at' => '2021-05-12 00:00:00',
+            'added_at' => '2021-05-12 00:00:00',
         ]);
 
         Transaction::factory()->create([
             'customer_name' => 'Customer 7',
             'total_price' => 40500,
             'items_quantity' => 5,
-            'completed_at' => '2021-05-12 00:00:00',
+            'added_at' => '2021-05-12 00:00:00',
         ]);
     }
 }
